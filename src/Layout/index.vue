8302d367090e81ad689b0b95615520dccaa8df5a
霍庆祝 <1533132103@qq.com>
2022-05-05 13:29:57 +0800

    修改

--- src/Layout/index.vue
+++ src/Layout/index.vue
@@ -1,16 +1,17 @@
 <template>
-  <Layout :list="routerList" title="信息化项目全流程管理"  />
+  <Layout :list="routerList" title="信息化项目全流程管理"  @select="select" :default-active="activeIndex" />
 </template>
 <script lang="ts" setup>
 import {Layout} from "@m/Layout"
 import {Edit,Avatar,Football} from "@element-plus/icons-vue"
-import {computed} from "vue"
-import { useRouter } from "vue-router";
+import {computed,ref} from "vue"
+import { useRouter, } from "vue-router";
 
 const props = defineProps({
   rootRoutePath: { type: String, required: true },
   systemTitle:  { type: String, required: true },
 })
+const activeIndex = ref()
 const list:any[]=[
   {
     path: "/test",
@@ -33,8 +34,15 @@ const list:any[]=[
 ]
 const router=useRouter()
 const routerList=computed(()=>{
+  //@ts-ignore
   let list = router?.options.routes.find((x) => x.path == props.rootRoutePath).children;
       console.log(list);
       return list||[];
 })
+const select =(index:string,param2:string[])=>{
+  console.log(index,param2);
+  activeIndex.value=index
+  const path= `${props.rootRoutePath}/${param2.join("/")}`
+  router.push(path)
+}
 </script>
